951ac50579290252bcb41ebe878ecdbf4c1fbdd1
softdev-nic <softtechon.inc@gmail.com>
2026-05-25 17:45:58 +0530

    invite acceptance

---
+++ src/Pages/AcceptInvite.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import React from 'react'
+import API from '../services/api'
+import { useParams } from 'react-router-dom'
+import { useEffect,useState } from 'react'
+function AcceptInvite() {
+  const { inviteToken } = useParams()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+   try{
+    const response = API.post(`/accept-invite/${inviteToken}`)
+   } catch(error){
+    console.error('Error accepting invite:', error)
+   }
+  }, [inviteToken])
+  return (
+    <div>
+      {loading?(<h1>Accepting Invite...</h1>):(<h1>Invite Accepted!</h1>)}
+    </div>
+    
+  )
+}
+
+export default AcceptInvite
